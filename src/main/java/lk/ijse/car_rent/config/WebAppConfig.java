307ddaba8bfb75517c8f80f859654822d32b6ce2
trish6028 <trishanpriyasachin@gmail.com>
2022-07-08 14:54:09 +0530

    first commit

---
+++ src/main/java/lk/ijse/car_rent/config/WebAppConfig.java
@@ -0,0 +1,26 @@
+package lk.ijse.car_rent.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.car_rent")
+
+
+public class WebAppConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+    @Bean
+    public MultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
+    }
+
+}
