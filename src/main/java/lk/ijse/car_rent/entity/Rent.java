814c06023f1087ca119c06fdf359e86172a58461
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 18:14:01 +0530

    update rent entity

--- src/main/java/lk/ijse/car_rent/entity/Rent.java
+++ src/main/java/lk/ijse/car_rent/entity/Rent.java
@@ -1,4 +1,26 @@
 package lk.ijse.car_rent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+
 public class Rent {
+    @Id
+    private String rId;
+    private String uId;
+    private String vId;
+    private double payment;
+    private String distance ;
+    private double refund;
+    private double date;
 }
