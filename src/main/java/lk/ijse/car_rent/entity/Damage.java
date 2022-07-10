d13e0d94d9645194ddea6842f8935ada3bf5f029
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 18:07:14 +0530

    update damage entity

--- src/main/java/lk/ijse/car_rent/entity/Damage.java
+++ src/main/java/lk/ijse/car_rent/entity/Damage.java
@@ -1,4 +1,23 @@
 package lk.ijse.car_rent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+
 public class Damage {
+    @Id
+    private String damId;
+    private String vId;
+    private double fee;
+    private String reason;
 }
