36d65980d16a4e354d4790c23b938140d117edac
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 19:49:59 +0530

    create Rentservice

--- src/main/java/lk/ijse/car_rent/service/RentService.java
+++ src/main/java/lk/ijse/car_rent/service/RentService.java
@@ -1,7 +1,15 @@
 package lk.ijse.car_rent.service;
 
 
+import lk.ijse.car_rent.dto.RentDTO;
+
+import java.util.List;
 
 public interface RentService {
+    void saveRent(RentDTO dto);
+    void deleteRent(String rId);
+    void updateRent(RentDTO dto);
+    void  searchRent(String rId);
+    List<RentDTO> getAllRents();
 
 }
