2eaf0a9981b838ee719ff2f2941e2240405e37b2
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 18:02:09 +0530

    admin entity update

--- src/main/java/lk/ijse/car_rent/entity/Admin.java
+++ src/main/java/lk/ijse/car_rent/entity/Admin.java
@@ -1,4 +1,24 @@
 package lk.ijse.car_rent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
 public class Admin {
+    @Id
+    private String id;
+    private String name;
+    private String address;
+    private String contact ;
+    private String email;
+
 }
