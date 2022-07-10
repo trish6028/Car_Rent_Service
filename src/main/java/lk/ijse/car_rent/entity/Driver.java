704fcf4d55020cfb6688349c835805bd134dea05
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 18:09:18 +0530

    create driver entity

---
+++ src/main/java/lk/ijse/car_rent/entity/Driver.java
@@ -0,0 +1,18 @@
+package lk.ijse.car_rent.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+
+public class Driver {
+
+}
