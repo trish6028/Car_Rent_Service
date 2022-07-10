848d943f7b1f3fed6368cf691b3b4f488fdadd51
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 19:46:26 +0530

    create Driverservice

--- src/main/java/lk/ijse/car_rent/service/DriverService.java
+++ src/main/java/lk/ijse/car_rent/service/DriverService.java
@@ -2,6 +2,16 @@ package lk.ijse.car_rent.service;
 
 
 
+import lk.ijse.car_rent.dto.DriverDTO;
+
+import java.util.List;
+
 public interface DriverService {
+    void saveDriver(DriverDTO dto);
+    void deleteDriver( String id);
+    void updateDamage(DriverDTO dto);
+    DriverDTO searchDriver(String id);
+    List<DriverDTO> getAllDrivers();
+
 
 }
