c2e5d72757423a40c3fb6f131c0aa8ac7bd53a67
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 18:17:16 +0530

    update reserv entity

--- src/main/java/lk/ijse/car_rent/entity/Reserv.java
+++ src/main/java/lk/ijse/car_rent/entity/Reserv.java
@@ -1,5 +1,26 @@
 package lk.ijse.car_rent.entity;
 
-public class Reserv {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+public class Reserv {
+    @Id
+    private String reservId;
+    private String uId;
+    private String vId;
+    private String dId;
+    private double resDate;
+    private double resTime;
+    private double pickupDate;
+    private double returnDate;
 }
