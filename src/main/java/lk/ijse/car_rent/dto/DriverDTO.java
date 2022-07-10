5e19913d1ebeb9dd066dbaac75f4fdcfecc9b874
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 14:35:40 +0530

    update DriverDto

--- src/main/java/lk/ijse/car_rent/dto/DriverDTO.java
+++ src/main/java/lk/ijse/car_rent/dto/DriverDTO.java
@@ -1,4 +1,21 @@
 package lk.ijse.car_rent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+
 public class DriverDTO {
+    private String id;
+    private String name;
+    private String address;
+    private String contact ;
+    private int licence;
+    private int NIC;
+    private String dImg;
 }
