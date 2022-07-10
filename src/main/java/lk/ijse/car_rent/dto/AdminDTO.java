48f93586c7132b7cc6f983f8400502a489ae87f8
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 14:13:14 +0530

    end AdminDTO

--- src/main/java/lk/ijse/car_rent/dto/AdminDTO.java
+++ src/main/java/lk/ijse/car_rent/dto/AdminDTO.java
@@ -1,4 +1,21 @@
 package lk.ijse.car_rent.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data // apply getters and setters
+@ToString
 public class AdminDTO {
+    private String id;
+    private String name;
+    private String address;
+    private String contact ;
+    private String email;
+
+
 }
