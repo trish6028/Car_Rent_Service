182080c1a31e5b5be39c9c3ca332741841066193
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 20:01:29 +0530

    create Userservice

--- src/main/java/lk/ijse/car_rent/service/UserService.java
+++ src/main/java/lk/ijse/car_rent/service/UserService.java
@@ -1,4 +1,15 @@
 package lk.ijse.car_rent.service;
 
+
+import lk.ijse.car_rent.dto.UserDTO;
+
+import java.util.List;
+
 public interface UserService {
+    void saveUser(UserDTO dto);
+    void deleteUser(String uId);
+    void updateUser(UserDTO dto);
+    void  searchUser(String uId);
+    List<UserDTO> getAllUsers();
+
 }
