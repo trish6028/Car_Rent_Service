82afa1824b5a7677440efaed03c97221b11ef8cd
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 16:04:01 +0530

    update RentDTO

--- src/main/java/lk/ijse/car_rent/dto/RentDTO.java
+++ src/main/java/lk/ijse/car_rent/dto/RentDTO.java
@@ -1,4 +1,21 @@
 package lk.ijse.car_rent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class RentDTO {
+    private String rId;
+    private String uId;
+    private String vId;
+    private double payment;
+    private String distance ;
+    private double refund;
+    private double date;
+
 }
