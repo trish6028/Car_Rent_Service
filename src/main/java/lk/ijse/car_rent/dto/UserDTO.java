d9fdd25ed7ab0aab16f45ddd7720a80f0f88f408
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 15:57:15 +0530

    update userDTO

--- src/main/java/lk/ijse/car_rent/dto/UserDTO.java
+++ src/main/java/lk/ijse/car_rent/dto/UserDTO.java
@@ -1,5 +1,21 @@
 package lk.ijse.car_rent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+
 public class UserDTO {
+    private String uId;
+    private String name;
+    private String address;
+    private String contact ;
+    private String email;
+
 
 }
