223e1574a30a5fc984695b86e6bc910cc3c68ecb
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 18:20:08 +0530

    update user entity

--- src/main/java/lk/ijse/car_rent/entity/User.java
+++ src/main/java/lk/ijse/car_rent/entity/User.java
@@ -1,4 +1,25 @@
 package lk.ijse.car_rent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+
 public class User {
+    @Id
+    private String uId;
+    private String name;
+    private String address;
+    private String contact ;
+    private String email;
+
 }
