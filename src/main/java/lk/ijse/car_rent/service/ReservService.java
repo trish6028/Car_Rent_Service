c03502d4fe21d27dbdeb7d3fb4e5dc6b9f024fe5
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 19:57:13 +0530

    create Reservservice

--- src/main/java/lk/ijse/car_rent/service/ReservService.java
+++ src/main/java/lk/ijse/car_rent/service/ReservService.java
@@ -1,4 +1,17 @@
 package lk.ijse.car_rent.service;
 
+
+
+import lk.ijse.car_rent.dto.ReservDTO;
+
+import java.util.List;
+
 public interface ReservService {
+    void saveReserv(ReservDTO dto);
+    void deleteReserv(String reservId);
+    void updateReserv(ReservDTO dto);
+    void  searchReserv(String reservId);
+    List<ReservDTO> getAllReservs();
+
+
 }
