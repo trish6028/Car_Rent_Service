e9a9c322f1b8e44ee8cd31039b653a9b5ae8826e
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 14:26:00 +0530

    end VehicleDto

--- src/main/java/lk/ijse/car_rent/dto/VehicleDTO.java
+++ src/main/java/lk/ijse/car_rent/dto/VehicleDTO.java
@@ -1,4 +1,19 @@
 package lk.ijse.car_rent.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class VehicleDTO {
+    private String vId;
+    private String brand;
+    private String type;
+    private String fuelType ;
+    private String transType;
+
 }
