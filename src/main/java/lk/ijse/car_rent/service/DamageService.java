df435db2013036b9165f16c3accedf2b06c73325
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 19:41:04 +0530

    create Damageservice

--- src/main/java/lk/ijse/car_rent/service/DamageService.java
+++ src/main/java/lk/ijse/car_rent/service/DamageService.java
@@ -2,7 +2,16 @@ package lk.ijse.car_rent.service;
 
 
 
+import lk.ijse.car_rent.dto.DamageDTO;
+
+import java.util.List;
+
 public interface DamageService {
+    void saveDamage(DamageDTO dto);
+    void deleteDamage(String damId);
+    void updateDamage(DamageDTO dto);
+    DamageDTO searchDamage(String damId);
+    List<DamageDTO> getAllDamages();
 
 
 }
