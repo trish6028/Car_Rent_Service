a8cffd3dfa73ba7ea7a89ff18aa86db37ef8a154
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 20:03:16 +0530

    create Vehicleservice

--- src/main/java/lk/ijse/car_rent/service/VehicleService.java
+++ src/main/java/lk/ijse/car_rent/service/VehicleService.java
@@ -1,5 +1,14 @@
 package lk.ijse.car_rent.service;
 
+import lk.ijse.car_rent.dto.VehicleDTO;
+
+import java.util.List;
+
 public interface VehicleService {
+    void saveVehicle(VehicleDTO dto);
+    void deleteVehicle(String vId);
+    void updateVehicle(VehicleDTO dto);
+    void  searchVehicle(String vId);
+    List<VehicleDTO> getAllVehicles();
 
 }
