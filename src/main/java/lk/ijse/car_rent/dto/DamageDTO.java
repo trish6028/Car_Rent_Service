8ee76e0d482f12664d70e54570a6923e6eead0b4
trish6028 <trishanpriyasachin@gmail.com>
2022-07-10 15:46:40 +0530

    update DamageDto

--- src/main/java/lk/ijse/car_rent/dto/DamageDTO.java
+++ src/main/java/lk/ijse/car_rent/dto/DamageDTO.java
@@ -1,4 +1,20 @@
 package lk.ijse.car_rent.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+
 public class DamageDTO {
+    private String damId;
+    private String vId;
+    private double fee;
+    private String reason;
+
 }
